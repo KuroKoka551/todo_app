ea9546390edff4490e975c7f5790d1f8339f81b8
KuroAngel <145038102+KuroKoka551@users.noreply.github.com>
2024-08-28 07:00:15 +0500

    graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	_ "github.com/lib/pq"
 
@@ -16,7 +21,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("failed to close database: %v", err)
+		}
+	}()
 	service := todoService{db: db}
 
 	gin.SetMode(gin.ReleaseMode)
@@ -27,5 +36,22 @@ func main() {
 	router.PUT("/todos/:id", service.UpdateTodo)
 	router.DELETE("/todos/:id", service.DeleteTodo)
 
-	log.Fatal(router.Run(config.Addr))
+	srv := http.Server{Addr: config.Addr, Handler: router}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Panicf("listen error: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	log.Println("Starting server, press CTRL+C to stop")
+	<-quit
+	log.Println("Shutting down server...")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Printf("Server shutdown error: %s\n", err)
+	}
+
+	log.Println("Server exited")
 }
